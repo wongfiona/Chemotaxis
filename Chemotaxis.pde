f5563cf02c5c889bca215a38148a65e90fb80a3c
wongfiona <wong.fiona.11@gmail.com>
2015-09-18 10:45:22 -0700

    Opacity

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -5,10 +5,10 @@ Bacteria [] colony;
  {     
    //initialize bacteria variables here   
    size(500, 500);
-   colony = new Bacteria [20];
+   colony = new Bacteria [100];
    for (int i = 0; i < colony.length; i++)
    {
-     colony[i] = new Bacteria();
+    colony[i] = new Bacteria();
    }
  }   
 
@@ -18,31 +18,36 @@ Bacteria [] colony;
    background(0);
    for (int i = 0; i < colony.length; i++)
    {
-     colony[i].move();
-     colony[i].show();
+    colony[i].move();
+    colony[i].show();
    }
  }  
 
  class Bacteria    
- {     
-   //lots of java! 
-   int myX, myY, myColor;
+ {      
+   int myX, myY, myR, myG, myB, myColor;
    Bacteria()
    {
-     myX = (int)(Math.random()*500);
-     myY = (int)(Math.random()*500);
-     myColor = (int)(Math.random()*255);
+    myX = (int)(Math.random()*500);
+    myY = (int)(Math.random()*500);
+    myR = (int)(Math.random()*500);
+    myG = (int)(Math.random()*500);
+    myB = (int)(Math.random()*500);
+    myColor = color (myR, myG, myB);
    }
 
    void move()
    {
-     myX = myX + (int)(Math.random()*3)-1;
-     myY = myY + (int)(Math.random()*3)-1;
+    myX = myX + (int)(Math.random()*3)-1;
+    myY = myY + (int)(Math.random()*3)-1;
    }
 
    void show()
    {
-     fill(myColor);
-     ellipse(myX, myY, 10, 10);
+   	stroke(myColor);
+    fill(myColor, 125);
+    ellipse(myX, myY, 10, 10);
+    fill(myColor, 150);
+    ellipse(myX, myY, 20, 20);
    }
  }      
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -21,10 +21,10 @@ Bacteria [] colony;
  {     
    //initialize bacteria variables here   
    size(500, 500);
-   colony = new Bacteria [20];
+   colony = new Bacteria [100];
    for (int i = 0; i < colony.length; i++)
    {
-     colony[i] = new Bacteria();
+    colony[i] = new Bacteria();
    }
  }   
 
@@ -34,32 +34,37 @@ Bacteria [] colony;
    background(0);
    for (int i = 0; i < colony.length; i++)
    {
-     colony[i].move();
-     colony[i].show();
+    colony[i].move();
+    colony[i].show();
    }
  }  
 
  class Bacteria    
- {     
-   //lots of java! 
-   int myX, myY, myColor;
+ {      
+   int myX, myY, myR, myG, myB, myColor;
    Bacteria()
    {
-     myX = (int)(Math.random()*500);
-     myY = (int)(Math.random()*500);
-     myColor = (int)(Math.random()*255);
+    myX = (int)(Math.random()*500);
+    myY = (int)(Math.random()*500);
+    myR = (int)(Math.random()*500);
+    myG = (int)(Math.random()*500);
+    myB = (int)(Math.random()*500);
+    myColor = color (myR, myG, myB);
    }
 
    public void move()
    {
-     myX = myX + (int)(Math.random()*3)-1;
-     myY = myY + (int)(Math.random()*3)-1;
+    myX = myX + (int)(Math.random()*3)-1;
+    myY = myY + (int)(Math.random()*3)-1;
    }
 
    public void show()
    {
-     fill(myColor);
-     ellipse(myX, myY, 10, 10);
+   	stroke(myColor);
+    fill(myColor, 125);
+    ellipse(myX, myY, 10, 10);
+    fill(myColor, 150);
+    ellipse(myX, myY, 20, 20);
    }
  }      
   static public void main(String[] passedArgs) {
